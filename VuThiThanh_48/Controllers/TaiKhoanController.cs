d5086b2e1730028c00e13e80c68bd36ff7a64063
2 Thanh Vũ Thị <1821051103@student.humg.edu.vn>
2021-06-06 15:51:44 +0700

    ADC

--- VuThiThanh_48/Controllers/TaiKhoanController.cs
+++ VuThiThanh_48/Controllers/TaiKhoanController.cs
@@ -9,9 +9,47 @@ namespace VuThiThanh_48.Controllers
     public class TaiKhoanController : Controller
     {
         // GET: TaiKhoan
-        public ActionResult TaiKhoan()
+        public ViewResult DangNhap(String returnUrl)
         {
+            ViewBag.returnUrl = returnUrl;
             return View();
         }
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult DangNhap(TAIKHOAN acc, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (acc.Username == "admin" && acc.Password == "123123")
+                {
+                    FormsAuthentication.SetAuthCookie(acc.Username, true);
+                    return RedirectToLocal(returnUrl);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thông tin đăng nhập không chính xác");
+                }
+
+            }
+            return View(acc);
+        }
+        public ActionResult DangXuat()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+        private ActionResult RedirectToLocal(String returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+
     }
 }
